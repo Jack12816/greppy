629b58856b7921d3757de88df4e5f59ba238dd49
Hermann Mayer <jack@hermann-mayer.net>
2013-08-13 09:31:43 +0200

    [Stores][Helper][Config] Implemented generic store class. Implemented helper and config concepts.

---
+++ docs/examples/extension/datatype.js
@@ -0,0 +1,13 @@
+/**
+ * Show some extensions on some native objects
+ */
+console.log(
+    [77, 3, 45, 54, 7].uniq(),
+    'crazy-shit'.toCamelCase(),
+    'hello World'.capitalize()
+);
+
+// [ 3, 7, 45, 54, 77 ]
+// 'crazyShit'
+// 'Hello World'
+
