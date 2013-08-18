2fff9223795989295d6f664cc9418ca9ada9e404
Hermann Mayer <jack@hermann-mayer.net>
2013-08-18 17:11:02 +0200

    [DB-Store] Implemented the DB-Store and also the mysql adapter (plain+ORM).

---
+++ docs/examples/db/mysql.js
@@ -0,0 +1,15 @@
+/**
+ * Get a plain MySQL Connection
+ */
+greppy.db.get('mysql.demo').instance.getConnection(function(err, con) {
+    console.log(con);
+});
+
+/**
+ * Get the MySQL ORM - Sequelize
+ */
+greppy.db.get('mysql.demo').getORM(function(orm, models) {
+    console.log(orm);
+    console.log(models);
+});
+
