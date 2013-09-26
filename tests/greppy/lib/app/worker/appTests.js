43226ca7aaf71e01c5a0ca890074bf83e464f3b2
Nabil Krause <nabil.krause@silberlicht.eu>
2013-09-26 18:02:48 +0200

    [Tests] Added new tests and extended an old one.

--- tests/greppy/lib/app/worker/appTests.js
+++ tests/greppy/lib/app/worker/appTests.js
@@ -18,6 +18,12 @@ describe('app', function() {
     
     beforeEach(function () {
         exApp = express();
+        
+        // setting global needed by some tests
+        logger  = {
+            debug: function(s) {
+            }
+        };
     });
     
     function getFuncName(fun) {
@@ -43,22 +49,23 @@ describe('app', function() {
     
     it('should only allow digits for id-parameters', function(done) {
         
-        /*var contextMockup = {
+        var contextMockup = {
             routes: [
                 {
                     path: '/just/some/test/',
                     method: 'get'
                 }
             ]
-        };*/
+        };
         
-        app = new App();
+        //app = new App();
+        app = new App(contextMockup);
         
         app.configure(exApp, null, null);
         
         // assume the last element in the stack is the greppy middleware.
         // remove it, so our test will run.
-        exApp.stack.pop();
+        //exApp.stack.pop();
         
         exApp.get('/just/some/test/:id', function(req, res) {
             res.send(req.params.id);
@@ -72,6 +79,46 @@ describe('app', function() {
                 
                 request(exApp)
                     .get('just/some/test/def')
+                    .end(function(err, res) {
+                        should.exist(err);
+                        should.not.exist(res);
+                        done();
+                });
+        });
+    });
+    
+    it('should only allow valid uuids for uuid-parameters', function(done) {
+        
+        var contextMockup = {
+            routes: [
+                {
+                    path: '/another/test',
+                    method: 'get'
+                }
+            ]
+        };
+        
+        //app = new App();
+        app = new App(contextMockup);
+        
+        app.configure(exApp, null, null);
+        
+        // assume the last element in the stack is the greppy middleware.
+        // remove it, so our test will run.
+        //exApp.stack.pop();
+        
+        exApp.get('/another/test/:uuid', function(req, res) {
+            res.send(req.params.uuid);
+        });
+        
+        request(exApp)
+            .get('/another/test/550e8400-e29b-11d4-a716-446655440000')
+            .end(function(err, res) {
+                should.not.exist(err);
+                res.text.should.equal('550e8400-e29b-11d4-a716-446655440000');
+                
+                request(exApp)
+                    .get('another/test/5508400-e29b-11d4-a716-446655440000')
                     .end(function(err, res) {
                         should.exist(err);
                         should.not.exist(res);
