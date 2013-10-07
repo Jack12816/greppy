bebc53ac3b888f8d1f60b13b0bb57d59821f8d76
Nabil Krause <nabil.krause@silberlicht.eu>
2013-10-07 18:24:14 +0200

    [Tests/data-grid] Fixed/Extended tests. Made method- and var-names of data-grid more clear.

--- tests/greppy/lib/app/worker/appTests.js
+++ tests/greppy/lib/app/worker/appTests.js
@@ -7,48 +7,52 @@
 var should  = require('should');
 var path    = require('path');
 var request = require('supertest');
+var colors  = require('colors');
 var root    = path.resolve('./');
 var express = require('express');
 var App     = require(root + '/lib/app/worker/app');
 
 describe('app', function() {
-    
+
     var exApp = null;
     var app   = null;
-    
+
     beforeEach(function () {
         exApp = express();
-        
+
         // setting global needed by some tests
         logger  = {
             debug: function(s) {
             }
         };
     });
-    
+
     function getFuncName(fun) {
         return /\W*function\s+([\w\$]+)\(/.exec(fun.toString())[1];
     }
-    
+
     it('should let the app use the body parser when it\'s configure method is called', function() {
-        
+
+        var contextMockup = {
+            routes: []
+        };
         var hasBodyParser = false;
-        
-        app = new App();
-        
+
+        app = new App(contextMockup);
+
         app.configure(exApp, null, null);
-        
+
         hasBodyParser = exApp.stack.some(function(mw) {
             if ('bodyParser' === getFuncName(mw.handle)) {
                 return true;
             }
         });
-        
+
         hasBodyParser.should.be.true;
     });
-    
+
     it('should only allow digits for id-parameters', function(done) {
-        
+
         var contextMockup = {
             routes: [
                 {
@@ -57,38 +61,34 @@ describe('app', function() {
                 }
             ]
         };
-        
-        //app = new App();
+
         app = new App(contextMockup);
-        
+
         app.configure(exApp, null, null);
-        
-        // assume the last element in the stack is the greppy middleware.
-        // remove it, so our test will run.
-        exApp.stack.pop();
-        
+
         exApp.get('/just/some/test/:id', function(req, res) {
             res.send(req.params.id);
         });
-        
+
         request(exApp)
             .get('/just/some/test/456')
             .end(function(err, res) {
                 should.not.exist(err);
                 res.text.should.equal('456');
-                
+
                 request(exApp)
-                    .get('just/some/test/def')
+                    .get('/just/some/test/def')
                     .end(function(err, res) {
-                        should.exist(err);
-                        should.not.exist(res);
+                        should.not.exist(err);
+                        res.statusCode.should.equal(404);
+                        res.text.toLowerCase().should.include('cannot');
                         done();
                 });
         });
     });
-    
+
     it('should only allow valid uuids for uuid-parameters', function(done) {
-        
+
         var contextMockup = {
             routes: [
                 {
@@ -97,36 +97,69 @@ describe('app', function() {
                 }
             ]
         };
-        
-        //app = new App();
+
         app = new App(contextMockup);
-        
+
         app.configure(exApp, null, null);
-        
-        // assume the last element in the stack is the greppy middleware.
-        // remove it, so our test will run.
-        exApp.stack.pop();
-        
+
         exApp.get('/another/test/:uuid', function(req, res) {
             res.send(req.params.uuid);
         });
-        
+
         request(exApp)
             .get('/another/test/550e8400-e29b-11d4-a716-446655440000')
             .end(function(err, res) {
                 should.not.exist(err);
                 res.text.should.equal('550e8400-e29b-11d4-a716-446655440000');
-                
+
                 request(exApp)
-                    .get('another/test/5508400-e29b-11d4-a716-446655440000')
+                    .get('/another/test/5508400-e29b-11d4-a716-446655440000')
                     .end(function(err, res) {
-                        should.exist(err);
-                        should.not.exist(res);
+                        should.not.exist(err);
+                        res.statusCode.should.equal(404);
+                        res.text.toLowerCase().should.include('cannot');
                         done();
                 });
         });
     });
-    
-    
+
+    it('should only allow valid oids for oid-parameters', function(done) {
+
+        var contextMockup = {
+            routes: [
+                {
+                    path: '/ok/then',
+                    method: 'get'
+                }
+            ]
+        };
+
+        app = new App(contextMockup);
+
+        app.configure(exApp, null, null);
+
+        exApp.get('/ok/then/:oid', function(req, res) {
+            res.send(req.params.oid);
+        });
+
+        request(exApp)
+            .get('/ok/then/fa54c3f7fcaa453dcd45f449')
+            .end(function(err, res) {
+                should.not.exist(err);
+                res.statusCode.should.equal(200);
+                res.text.should.equal('fa54c3f7fcaa453dcd45f449');
+
+                request(exApp)
+                    .get('/ok/then/fdsfsdfewgrt54')
+                    .end(function(err, res) {
+                        should.not.exist(err);
+                        res.statusCode.should.equal(404);
+                        res.text.toLowerCase().should.include('cannot');
+                        done();
+                });
+        });
+    });
+
+
 });
 
--- tests/greppy/lib/helper/controller/data-gridTests.js
+++ tests/greppy/lib/helper/controller/data-gridTests.js
@@ -1,6 +1,6 @@
 /**
  * Tests for /lib/helper/controller/data-grid
- * 
+ *
  * @author Nabil Krause <nabil.krause@silberlicht.eu>
  */
 
@@ -11,64 +11,64 @@ var DataGrid   = require(root + '/lib/helper/controller/data-grid');
 var dg   = null;
 
 describe('Data-Grid', function() {
-    
+
     beforeEach(function() {
-        
+
         dg = new DataGrid();
     });
-    
-    describe('buildCriteria', function() {
-        
+
+    describe('buildBaseCriteria', function() {
+
         it('should return a critertia object with default values, if the req has no special values', function() {
-            
+
             var reqMockup = {
                 query: {}
             };
-            
-            var result = dg.buildCriteria(reqMockup, {}, {});
-            
+
+            var result = dg.buildBaseCriteria(reqMockup, {}, {});
+
             result.view.should.equal('index');
             result.limit.should.equal(25);
             result.offset.should.equal(0);
             result.page.should.equal(1);
             result.pageSizes.should.eql([10, 25, 50, 100]);
         });
-        
+
         it('should should return a criteria object with special values, if options were passed', function() {
-            
+
             var reqMockup = {
                 query: {}
             };
-            
-            var result = dg.buildCriteria(reqMockup, {}, {
+
+            var result = dg.buildBaseCriteria(reqMockup, {}, {
                 limit: 123,
                 pageSizes: [1, 2, 3, 4]
-            })
-            
+            });
+
             result.limit.should.equal(123);
             result.pageSizes.should.eql([1, 2, 3, 4]);
         });
-        
+
         it('should return a criteria object with custom page, limit and offset, based on the values of the query', function() {
-            
+
             var reqMockup = {
                 query: {
                     page: 12,
                     limit: 500
                 }
             };
-            
-            var result = dg.buildCriteria(reqMockup, {}, {});
-            
+
+            var result = dg.buildBaseCriteria(reqMockup, {}, {});
+
             result.view.should.equal('index');
             result.limit.should.equal(500);
             result.offset.should.equal(5500);
             result.page.should.equal(12);
             result.pageSizes.should.eql([10, 25, 50, 100]);
         });
-        
+
         it('should return a criteria object with the correct view-property, based on the render-value of the query', function() {
-            
+
             var reqMockup1 = {
                 query: {
                     render: 'rows'
@@ -81,8 +81,8 @@ describe('Data-Grid', function() {
                 }
             };
 
-            var result1 = dg.buildCriteria(reqMockup1, {}, {});
-            var result2 = dg.buildCriteria(reqMockup2, {}, {});
+            var result1 = dg.buildBaseCriteria(reqMockup1, {}, {});
+            var result2 = dg.buildBaseCriteria(reqMockup2, {}, {});
 
             result1.view.should.equal('_index_rows');
             result2.view.should.equal('_pagination');
@@ -107,9 +107,9 @@ describe('Data-Grid', function() {
             result.pageSizes.should.eql([10, 25, 50, 100]);
             result.where.should.equal('(deleted_at IS NULL)');
         });
-        
+
         it('should return a criteria object with the correct where clause for sql with non-fuzzy searching', function() {
-    
+
             var reqMockup = {
                 query: {
                     search: 'mySearchTerm',
@@ -125,11 +125,11 @@ describe('Data-Grid', function() {
 
             result.where.should.equal('(mySearchColumn LIKE \'%mySearchTerm%\') AND (deleted_at IS NULL)');
         });
-        
+
         it('should return a criteria object with the correct where clause for sql with fuzzy searching', function() {
-    
+
             // WIP
-    
+
             var reqMockup = {
                 query: {
                     search : 'mySearchTerm',
@@ -146,7 +146,7 @@ describe('Data-Grid', function() {
 
             result.where.should.equal('(mySearchColumn LIKE \'%mySearchTerm%\') AND (deleted_at IS NULL)');
         });
-        
+
     });
 });
 
